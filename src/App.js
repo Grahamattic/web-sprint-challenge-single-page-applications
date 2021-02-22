8a5d06c7641ce700646d6b5a3f5fa110587bce21
Shenica Graham <gshenica@gmail.com>
2021-02-21 22:39:57 -0600

    Add buttons to navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
 const App = () => {
   return (
-    <>
+    <div className='container'>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+      <button className="header-button">HOME</button>
+    </div>
   );
 };
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+// Import React libraries from node_modules folder
+    import React from "react";
+    import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// Import main stylesheet
+    import "./index.css";
+
+// Import top-level component
+    import App from "./App";
+
+// Display app
+    ReactDOM.render(<App />, document.getElementById("root"));
